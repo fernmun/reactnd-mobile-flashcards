44084e1269effe57e996c67c2a4b2b43cb8e7215
fernmun <lfmparra@gmail.com>
2017-11-17 18:11:36 -0500

    [fix] Can't enter data in TextInput :tada:

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux'
-import { reducer as formReducer } from 'redux-form'
+import { reducer as form } from 'redux-form'
 import decks from './decks'
 import questions from './questions'
 
 export default combineReducers({
   decks,
   questions,
-  formReducer
+  form
 })
